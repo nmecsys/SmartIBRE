6159c9f7338582cd0154d7f3919ad2cea6d4be7f
GreedBlink <johnmasterchip@gmail.com>
2017-08-22 20:16:51 -0300

    up

--- smartibre_free/global.R
+++ smartibre_free/global.R
@@ -12,6 +12,12 @@ library(DT)
 conn <- dbConnect(MySQL(), host = "200.20.164.178", db = "smartibredb", user = "smartibre_user", password = "123456", port = 3306)
 
 
+#funcao para adicionar aos favoritos
+
+add_fav<-function(series = c()){
+  sql = "insert into favoritos()"
+}
+
 #dbSendQuery(conn,"insert into favoritos(series) values('c(1,2,3)')")
 #supondo que conseguimos identificar o usuario, atraves do seu username
 # dbGetQuery(conn,"select * from favoritos where username = 'username'")
